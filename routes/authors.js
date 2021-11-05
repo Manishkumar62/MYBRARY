4cfd280e086df542605632a96d1348c080792951
Manish <vishwamanish@gmail.com>
2021-11-05 17:23:37 +0530

    Finish Book Routes

--- routes/authors.js
+++ routes/authors.js
@@ -41,6 +41,7 @@ router.post('/', async (req,res)=>{
     }
 })
 
+//Show Author Route
 router.get('/:id', async (req,res)=>{
     try{
         const author = await Author.findById(req.params.id)
@@ -54,6 +55,7 @@ router.get('/:id', async (req,res)=>{
     }
 })
 
+//Edit Author Route
 router.get('/:id/edit', async (req,res)=>{
     try{   
         const author = await Author.findById(req.params.id)     
@@ -63,6 +65,7 @@ router.get('/:id/edit', async (req,res)=>{
     }
 })
 
+//Update Author Route
 router.put('/:id', async (req,res)=>{
     let author
     try{
@@ -82,6 +85,7 @@ router.put('/:id', async (req,res)=>{
     }
 })
 
+//Delete Author Route
 router.delete('/:id', async (req,res)=>{
     let author
     try{
