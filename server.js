1c17170afab64dcbb79f676fc263d438ff613947
Manishkumar62 <vishwamanish62@gmail.com>
2023-10-02 18:25:20 +0530

    connected to cloud and dotenv

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const dotenv = require('dotenv')
 const expressLayouts = require("express-ejs-layouts")
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
 const aboutmeRouter = require('./routes/aboutme')
 const authorRouter = require('./routes/authors')
 const bookRouter = require('./routes/books')
 
+dotenv.config();
 app.set('view engine','ejs')
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
@@ -18,33 +20,16 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ limit:'10mb', extended:false }))
 
-dotenv.config()
-
-const mongoose = require('mongoose')
-const connectDb = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URL,{
-            useNewUrlParser:true
-        })
-        const db = mongoose.connection
-        db.on('error',error => console.error(error))
-        db.once('open',() => console.log('Connected to Mongoose'))
-    } catch (error) {
-        console.log(`${error}`.bgRed)
-    }
-}
-
-connectDb();
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`));
+    
 
 app.use('/',indexRouter)
 app.use('/aboutme',aboutmeRouter)
 app.use('/authors',authorRouter)
 app.use('/books',bookRouter)
-
-//port
-const PORT = 8080 || process.env.PORT
-
-//listen server
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
